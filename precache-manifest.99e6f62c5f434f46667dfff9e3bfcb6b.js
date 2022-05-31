34ac37fd8f75e32a61c424aebbca5e25f7701d65
Vivek Kumar <code.vivekkumar@gmail.com>
2022-05-31 19:23:38 +0530

    Updates

---
+++ precache-manifest.99e6f62c5f434f46667dfff9e3bfcb6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bcf855a2ba0a134b6f6cc997932b4a0",
+    "url": "/musyx/index.html"
+  },
+  {
+    "revision": "6a76178f5c76cb2cf5a2",
+    "url": "/musyx/static/css/main.ddf84d68.chunk.css"
+  },
+  {
+    "revision": "d8ce1a2e79c245946034",
+    "url": "/musyx/static/js/2.1c8724c5.chunk.js"
+  },
+  {
+    "revision": "b0382a1d868abef989ec0625e2034248",
+    "url": "/musyx/static/js/2.1c8724c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a76178f5c76cb2cf5a2",
+    "url": "/musyx/static/js/main.f300fab6.chunk.js"
+  },
+  {
+    "revision": "5ca7f967d104b89bbcb6",
+    "url": "/musyx/static/js/runtime-main.735af1e9.js"
+  }
+]);
